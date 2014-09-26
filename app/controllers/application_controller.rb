b1746adc290f5a70a2f294255f6ff437edcb775c
Terence Ponce <terenceponce@gmail.com>
2014-09-26 21:25:51 +0800

    Add authentication token for api usage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+    def authenticate_user_from_token!
+      user_email = params[:user_email].presence
+      user = user_email && User.where(email: user_email).first
+
+      if user && Devise.secure_compare(user.authentication_token, params[:user_token])
+        sign_in user, store: false
+      end
+    end
 end
