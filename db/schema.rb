4b14a85410350458dcca8f7aba3f79aaa566b162
Terence Ponce <terenceponce@gmail.com>
2014-10-11 12:25:03 +0800

    Add category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140927092942) do
+ActiveRecord::Schema.define(version: 20141011042042) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "speakers", force: true do |t|
     t.string   "first_name"
