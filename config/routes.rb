765907df3b83efaf182162186f699d37f4f11688
Terence Ponce <terenceponce@gmail.com>
2014-11-27 20:08:38 +0800

    Add current_user endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
   devise_for :users
   mount Devcon::Summit::API => '/api'
 
+  namespace :api do
+    namespace :v2 do
+      get '/me' => 'credentials#me'
+    end
+  end
+
   root 'pages#home'
 end
